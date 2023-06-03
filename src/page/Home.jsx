0bd819356e318c56b26b418d13c010249b7b9ae2
MatheusFoliatti <matheusfoliatti@gmail.com>
2023-06-02 21:04:05 -0300

    redbutton

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,18 +7,42 @@ import {
 } from "react-native";
 
 export function Home() {
+	function handleParticipanteAdd() {
+		console.log("Nossa função está funcionando");
+	}
+
+	function handleParticipanteDelete() {
+		console.log("Nossa função está funcionando");
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.titleEvent}> Nome do evento </Text>
 			<Text style={styles.dateEvent}> Sexta, 2 de Junho de 2023 </Text>
-			<TextInput
-				style={styles.input}
-				placeholder='Nome do participante...'
-				placeholderTextColor={"#6b6b6b"}
-			/>
-			<TouchableOpacity style={styles.button}>
-				<Text style={styles.addButton}> + </Text>
-			</TouchableOpacity>
+
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder='Nome do participante...'
+					placeholderTextColor={"#6b6b6b"}
+				/>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={handleParticipanteAdd}
+					activeOpacity={0.7}>
+					<Text style={styles.addButton}> + </Text>
+				</TouchableOpacity>
+			</View>
+
+			<Text style={styles.titleEvent}>Participantes</Text>
+			<View style={styles.form}>
+				<TouchableOpacity
+					style={styles.redButton}
+					onPress={handleParticipanteDelete}
+					activeOpacity={0.7}>
+					<Text style={styles.deleteButton}> - </Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
@@ -39,7 +63,15 @@ const styles = StyleSheet.create({
 		color: "#6b6b6b",
 		fontSize: 16,
 	},
+	form: {
+		width: "100%",
+		flexDirection: "row",
+		marginTop: 36,
+		marginBottom: 42,
+		gap: 7,
+	},
 	input: {
+		flex: 1,
 		height: 56,
 		backgroundColor: "#1f1e25",
 		borderRadius: 5,
@@ -56,6 +88,18 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 	},
 	addButton: {
+		color: "#fff",
+		fontSize: 24,
+	},
+	redButton: {
+		width: 56,
+		height: 56,
+		borderRadius: 5,
+		backgroundColor: "#E23C44",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	deleteButton: {
 		color: "#fff",
 		fontSize: 24,
 	},
