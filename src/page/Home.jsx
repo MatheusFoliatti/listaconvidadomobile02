9f44425376699ea690614ca6e30360d5601925a4
MatheusFoliatti <matheusfoliatti@gmail.com>
2023-06-06 21:56:02 -0300

    prettier

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,87 +1,87 @@
 import {
-	View,
-	StyleSheet,
-	Text,
-	TextInput,
-	TouchableOpacity,
-} from "react-native";
-import { Participantes } from "../components/Participantes";
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native'
+import { Participantes } from '../components/Participantes'
 
 export function Home() {
-	function handleParticipanteAdd() {
-		console.log("A função está funcionando");
-	}
+    function handleParticipanteAdd() {
+        console.log('A função está funcionando')
+    }
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.titleEvent}> Nome do evento </Text>
-			<Text style={styles.dateEvent}> Sexta, 2 de Junho de 2023 </Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titleEvent}> Nome do evento </Text>
+            <Text style={styles.dateEvent}> Sexta, 2 de Junho de 2023 </Text>
 
-			<View style={styles.form}>
-				<TextInput
-					style={styles.input}
-					placeholder="Nome do participante..."
-					placeholderTextColor={"#6b6b6b"}
-				/>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={handleParticipanteAdd}
-					activeOpacity={0.7}
-				>
-					<Text style={styles.addButton}> + </Text>
-				</TouchableOpacity>
-			</View>
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante..."
+                    placeholderTextColor={'#6b6b6b'}
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleParticipanteAdd}
+                    activeOpacity={0.7}
+                >
+                    <Text style={styles.addButton}> + </Text>
+                </TouchableOpacity>
+            </View>
 
-			<Participantes name="Sextou!!!!" />
-			<Participantes name="Sabadou!!!" />
-			<Participantes name="Domingou!!!" />
-			<Participantes name="Segundou :(" />
-		</View>
-	);
+            <Participantes name="Sextou!!!!" />
+            <Participantes name="Sabadou!!!" />
+            <Participantes name="Domingou!!!" />
+            <Participantes name="Segundou :(" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#131016",
-		padding: 24,
-	},
-	titleEvent: {
-		color: "#fdfcfe",
-		fontSize: 24,
-		fontWeight: "bold",
-		marginTop: 48,
-	},
-	dateEvent: {
-		color: "#6b6b6b",
-		fontSize: 16,
-	},
-	form: {
-		width: "100%",
-		flexDirection: "row",
-		marginTop: 36,
-		marginBottom: 42,
-		gap: 7,
-	},
-	input: {
-		flex: 1,
-		height: 56,
-		backgroundColor: "#1f1e25",
-		borderRadius: 5,
-		color: "#fff",
-		padding: 16,
-		fontSize: 16,
-	},
-	button: {
-		width: 56,
-		height: 56,
-		borderRadius: 5,
-		backgroundColor: "#31cf65",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	addButton: {
-		color: "#fff",
-		fontSize: 24,
-	},
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+    },
+    titleEvent: {
+        color: '#fdfcfe',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48,
+    },
+    dateEvent: {
+        color: '#6b6b6b',
+        fontSize: 16,
+    },
+    form: {
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42,
+        gap: 7,
+    },
+    input: {
+        flex: 1,
+        height: 56,
+        backgroundColor: '#1f1e25',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 16,
+        fontSize: 16,
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31cf65',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    addButton: {
+        color: '#fff',
+        fontSize: 24,
+    },
+})
